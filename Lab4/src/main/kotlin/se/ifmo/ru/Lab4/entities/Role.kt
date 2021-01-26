bd0552e4f03e734000c40e75fe27d26d764b04f0
alexmax100500 <57871619+alexmax100500@users.noreply.github.com>
2021-01-26 17:42:17 +0300

    Add files via upload
    User class updated, return all the points enabled

---
+++ Lab4/src/main/kotlin/se/ifmo/ru/Lab4/entities/Role.kt
@@ -0,0 +1,34 @@
+package se.ifmo.ru.Lab4.entities
+
+import javax.persistence.ManyToMany
+import org.springframework.security.core.GrantedAuthority
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+
+@Entity
+@Table(name = "t_role")
+class Role : GrantedAuthority {
+    @Id
+    var id: Long? = null
+    var name: String? = null
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    var users: Set<User>? = null
+
+    constructor() {}
+
+    constructor(id: Long?) {
+        this.id = id
+    }
+
+    constructor(id: Long?, name: String) {
+        this.id = id
+        this.name = name
+    }
+
+    override fun getAuthority(): String? {
+        return name
+    }
+}
