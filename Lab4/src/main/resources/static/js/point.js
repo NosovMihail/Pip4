b3f93aaea4c88d9d37a5d294fec4c768189f6a8f
NosovMihail <32123595+NosovMihail@users.noreply.github.com>
2021-01-27 12:14:16 +0300

    Add files via upload

--- Lab4/src/main/resources/static/js/point.js
+++ Lab4/src/main/resources/static/js/point.js
@@ -6,42 +6,92 @@
 // });
 var messageApi = Vue.resource('/point');
 var request = Vue.resource('/points');
-Vue.component('send', {
-    props: ['results'],
+
+
+Vue.component('message-form', {
+    props: ['messages', 'messageAttr'],
     data: function() {
         return {
-            X: '',
-            Y: '',
-            R: ''
+            text: '',
+            id: ''
+        }
+    },
+    watch: {
+        messageAttr: function(newVal, oldVal) {
+            this.text = newVal.text;
+            this.id = newVal.id;
         }
     },
     template:
-        '<div>'+
-        '<input type="button" value="Отправить" @click="save" />'+
+        '<div>' +
+        '<input type="button" value="Обновить" @click="save" />' +
         '</div>',
     methods: {
         save: function() {
-            var message = {x: this.x, y: this.y, r: this.r};
-            messageApi.get(message);
-            request.get().then(result => result.json().then(data => data.forEach(ans => this.results.push(ans))));
+            request.get().then(result =>
+                result.json().then(data => {
+                       this.messages.length = 0;
+                        data.forEach(message => this.messages.push(message))
+                    }
+                )
+            )
+            refrR();
         }
     }
 });
-var post = new Vue({
-   el:"#post",
-   template: '<send/>'
-});
-var res = new Vue({
-    el:"#res",
-    template: '<td v-for="result in res" :key=res>{{res}}</td>',
-    data:{
-        results: []
-    }
-})
-var app = new Vue({
-    // root node
-    el: "#app",
-    //template: '<login-form />',
+
+Vue.component('message-row', {
+    props: ['message', 'editMethod', 'messages'],
+    template: '<div>' +
+        '<i>({{ message.id }})</i> {{ message.owner }} {{ message.x }} {{ message.y }} {{ message.r }} {{ message.isCoordsStatus }} {{ message.bornDate }}' +
+        '<span style="position: absolute; right: 0">' +
+        '</span>' +
+        '</div>',
     methods: {
+        edit: function() {
+            this.editMethod(this.message);
+        },
+        del: function() {
+            request.remove({id: this.message.id}).then(result => {
+                if (result.ok) {
+                    this.messages.splice(this.messages.indexOf(this.message), 1)
+                }
+            })
+        }
+    }
+});
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    data: function() {
+        return {
+            message: null
+        }
     },
+    template:
+        '<div style="position: relative; width: 800px;">' +
+        '<message-form :messages="messages" :messageAttr="message" />' +
+        '<message-row v-for="message in messages" :key="message.id" :message="message" ' +
+        ':editMethod="editMethod" :messages="messages" />' +
+        '</div>',
+    created: function() {
+        request.get().then(result =>
+            result.json().then(data =>
+                data.forEach(message => this.messages.push(message))
+            )
+        )
+    },
+    methods: {
+        editMethod: function(message) {
+            this.message = message;
+        }
+    }
+});
+
+var app = new Vue({
+    el: '#app',
+    template: '<messages-list :messages="messages" />',
+    data: {
+        messages: []
+    }
 });
